0e4697ce3900c1b361570a5f7dc751d8d219ac6c
haroldluis09 <haroldluis09@gmail.com>
2025-05-29 15:23:25 -0400

    Sidebar

--- SidebarMenu/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('/SidebarMenu/sidebar.html')
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('sidebar-container').innerHTML = html;
-      })
-      .catch(err => console.error('Sidebar failed to load:', err));
-  });
